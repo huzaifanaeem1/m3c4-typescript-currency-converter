57fb554e5f1e67de3b9ac84c1b8c79bc5efe0ebe
Huzaifa Naeem <159990985+huzaifanaeem1@users.noreply.github.com>
2024-04-06 03:00:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let Conversion = {
+    "PKR": {
+        "USD": 0.0036,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    /// In GBP
+    "GBP": {
+        "USD": 1.21,
+        "PKR": 350,
+        "GBP": 1
+    },
+    // In Dollars
+    "USD": {
+        "PKR": 277.58,
+        "GBP": 0.83,
+        "USD": 1
+    },
+};
+/// using  Inquirer
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your currency.....?"
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your conversion currency.....?"
+    },
+    ////conversion amount
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter Your Conversion Amount"
+    }
+]);
+const { from, to, amount } = answer;
+/// check input in given programs
+if (from && to && amount) {
+    //formula
+    let result = Conversion[from][to] * amount;
+    console.log(`Your conversion from ${from} to ${to} is ${result}`);
+}
+else {
+    /// generate error
+    console.log("Invalid Input");
+}
